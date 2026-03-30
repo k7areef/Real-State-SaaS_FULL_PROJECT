e7e237b3b44725ed745cda93495ff0ff4b84fa6e
k7areef <kh3reef@gmail.com>
2026-03-30 07:20:56 +0200

    feat: update Footer component with AppLogo and copyright information - Integrated AppLogo into the Footer for branding consistency. - Enhanced layout with improved styling and added copyright notice in Arabic for better user experience.

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,8 +1,15 @@
+import AppLogo from "@components/common/AppLogo";
+
 function Footer() {
     return (
-        <footer>
-            <div className="container">
-                Footer
+        <footer className="py-4 border-t border-t-grey">
+            <div className="container flex items-center justify-between">
+                {/* App Logo */}
+                <AppLogo />
+                {/* Copyright */}
+                <div className="text-center text-sm">
+                    © {new Date().getFullYear()} عقاري. جميع الحقوق محفوظة.
+                </div>
             </div>
         </footer>
     )
